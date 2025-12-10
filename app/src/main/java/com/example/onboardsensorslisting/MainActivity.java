99bd4de3be792d67301e2de4931c13ed88d8ca05
Kiphea <djsmyth05@gmail.com>
2025-12-10 15:14:59 +0000

    Listing your available sensors

--- app/src/main/java/com/example/onboardsensorslisting/MainActivity.java
+++ app/src/main/java/com/example/onboardsensorslisting/MainActivity.java
@@ -2,7 +2,16 @@ package com.example.onboardsensorslisting;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.listView_sensors);
+        List sensorList = new ArrayList<String>();
+        List<Sensor> sensors = ((SensorManager) getSystemService(Context.SENSOR_SERVICE)).getSensorList(Sensor.TYPE_ALL);
+
+        for (Sensor sensor: sensors){
+            sensorList.add(sensor.getName());
+        }
+
+        ListAdapter sensorAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,sensorList);
+        listView.setAdapter(sensorAdapter);
     }
 }
